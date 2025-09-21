0a6bd04daeab15f975c197c96892e87fd8dcad74
inwoo <dlsdn1108@gmail.com>
2025-09-22 08:51:00 +0900

    db 위치 문제

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+
+DATA_DIR = "./data"
+os.makedirs(DATA_DIR, exist_ok=True)
 
 # SQLite DB 파일 위치
-SQLALCHEMY_DATABASE_URL = "sqlite:///./schedule.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/schedule.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
